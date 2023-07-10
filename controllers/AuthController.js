441a643966ea2f6c82867dfaf4629466a87a362f
Mayerling Joseph <josephmayerling@gmail.com>
2023-07-10 10:49:12 -0400

    Reading

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,44 +1,102 @@
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const User = require('../models/User')
-const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS)
-const APP_SECRET = process.env.APP_SECRET
+const { User } = require('../models')
+const middleware = require('../middleware')
 
-// User registration
-exports.registerUser = async (req, res) => {
-  const { email, password } = req.body
+const Register = async (req, res) => {
   try {
-    const existingUser = await User.findOne({ email })
+    // Extracts the necessary fields from the request body
+    const { email, password, name } = req.body
+    // Hashes the provided password
+    let passwordDigest = await middleware.hashPassword(password)
+    // Checks if there has already been a user registered with that email
+    let existingUser = await User.findOne({ email })
     if (existingUser) {
-      return res.status(409).json({ message: 'User already exists' })
+      return res
+        .status(400)
+        .send('A user with that email has already been registered!')
+    } else {
+      // Creates a new user
+      const user = await User.create({ name, email, passwordDigest })
+      // Sends the user as a response
+      res.send(user)
     }
-    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS)
-    const user = await User.create({ email, password: hashedPassword })
-    res.status(201).json({ user })
   } catch (error) {
-    console.error(error)
-    res.status(500).json({ message: 'Failed to register user' })
+    throw error
   }
 }
 
-// User login
-exports.loginUser = async (req, res) => {
-  const { email, password } = req.body
+const Login = async (req, res) => {
   try {
+    // Extracts the necessary fields from the request body
+    const { email, password } = req.body
+    // Finds a user by a particular field (in this case, email)
     const user = await User.findOne({ email })
-    if (!user) {
-      return res.status(401).json({ message: 'Invalid credentials' })
+    // Checks if the password matches the stored digest
+    let matched = await middleware.comparePassword(
+      user.passwordDigest,
+      password
+    )
+    // If they match, constructs a payload object of values we want on the front end
+    if (matched) {
+      let payload = {
+        id: user.id,
+        email: user.email
+      }
+      // Creates our JWT and packages it with our payload to send as a response
+      let token = middleware.createToken(payload)
+      return res.send({ user: payload, token })
     }
-    const isPasswordValid = await bcrypt.compare(password, user.password)
-    if (!isPasswordValid) {
-      return res.status(401).json({ message: 'Invalid credentials' })
-    }
-    const token = jwt.sign({ userId: user._id }, APP_SECRET, {
-      expiresIn: '1h'
-    })
-    res.json({ token })
+    res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
   } catch (error) {
-    console.error(error)
-    res.status(500).json({ message: 'Failed to login' })
+    console.log(error)
+    res.status(401).send({ status: 'Error', msg: 'An error has occurred!' })
   }
+}
+
+const UpdatePassword = async (req, res) => {
+  try {
+    // Extracts the necessary fields from the request body
+    const { oldPassword, newPassword } = req.body
+    // Finds a user by a particular field (in this case, the user's id from the URL param)
+    let user = await User.findById(req.params.user_id)
+    // Checks if the password matches the stored digest
+    let matched = await middleware.comparePassword(
+      user.passwordDigest,
+      oldPassword
+    )
+    // If they match, hashes the new password, updates the db with the new digest, then sends the user as a response
+    if (matched) {
+      let passwordDigest = await middleware.hashPassword(newPassword)
+      user = await User.findByIdAndUpdate(req.params.user_id, {
+        passwordDigest
+      })
+      let payload = {
+        id: user.id,
+        email: user.email
+      }
+      return res.send({ status: 'Password Updated!', user: payload })
+    }
+    res
+      .status(401)
+      .send({ status: 'Error', msg: 'Old Password did not match!' })
+  } catch (error) {
+    console.log(error)
+    res
+      .status(401)
+      .send({
+        status: 'Error',
+        msg: 'An error has occurred updating password!'
+      })
+  }
+}
+
+const CheckSession = async (req, res) => {
+  const { payload } = res.locals
+  res.send(payload)
+}
+
+module.exports = {
+  Register,
+  Login,
+  UpdatePassword,
+  CheckSession
 }
