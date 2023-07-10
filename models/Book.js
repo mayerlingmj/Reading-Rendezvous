5b522c39ea44f13985f6c45350601f64cb6eab52
Mayerling Joseph <josephmayerling@gmail.com>
2023-07-10 10:13:59 -0400

    Reading

--- models/Book.js
+++ models/Book.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const bookSchema = new mongoose.Schema({
-  title: { type: String, required: true },
+  title: { type: String, required: true, unique: true },
   author: { type: String, required: true },
   description: { type: String, required: true }
 })
