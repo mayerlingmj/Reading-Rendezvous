30cf8ef9bd5269aefbc404d0cf119792b34dc332
Mayerling Joseph <josephmayerling@gmail.com>
2023-07-12 11:51:50 -0400

    Reading

--- Frontend/src/components/Books.jsx
+++ Frontend/src/components/Books.jsx
@@ -1,4 +1,25 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
+const Books = () => {
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const result = await axios.get('/books')
+      setBooks(result.data)
+    }
+
+    fetchBooks()
+  }, [])
+
+  return (
+    <div>
+      {books.map((book, index) => (
+        <p key={index}>{book.title}</p>
+      ))}
+    </div>
+  )
+}
+
 export default Books
