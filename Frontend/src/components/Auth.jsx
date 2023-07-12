be2b514eadb847990f972cf0025bf28b6f6a402c
Mayerling Joseph <josephmayerling@gmail.com>
2023-07-12 10:59:37 -0400

    Reading

--- Frontend/src/components/Auth.jsx
+++ Frontend/src/components/Auth.jsx
@@ -1,79 +1,132 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 
 const Auth = () => {
-  const [loginEmail, setLoginEmail] = useState('')
-  const [loginPassword, setLoginPassword] = useState('')
-  const [registerEmail, setRegisterEmail] = useState('')
-  const [registerPassword, setRegisterPassword] = useState('')
+  let navigate = useNavigate()
+
+  const [formValues, setFormValues] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  })
+
+  const handleChange = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value })
+  }
 
   const handleLogin = async (e) => {
     e.preventDefault()
-    // add login logic here
     const result = await axios.post('/auth/login', {
-      email: loginEmail,
-      password: loginPassword
+      email: formValues.email,
+      password: formValues.password
     })
     console.log(result.data)
+    setFormValues({ email: '', password: '' })
+    navigate('/feed')
   }
 
   const handleRegister = async (e) => {
     e.preventDefault()
-    // add register logic here
     const result = await axios.post('/auth/register', {
-      email: registerEmail,
-      password: registerPassword
+      name: formValues.name,
+      email: formValues.email,
+      password: formValues.password
     })
     console.log(result.data)
+    setFormValues({ name: '', email: '', password: '', confirmPassword: '' })
+    navigate('/signin')
   }
 
   return (
-    <div>
+    <div className="signin col">
       <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-        <label>
-          Email:
-          <input
-            type="email"
-            value={loginEmail}
-            onChange={(e) => setLoginEmail(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Password:
-          <input
-            type="password"
-            value={loginPassword}
-            onChange={(e) => setLoginPassword(e.target.value)}
-            required
-          />
-        </label>
-        <button type="submit">Login</button>
-      </form>
+      <div className="card-overlay centered">
+        <form className="col" onSubmit={handleLogin}>
+          <div className="input-wrapper">
+            <label htmlFor="email">Email</label>
+            <input
+              onChange={handleChange}
+              name="email"
+              type="email"
+              placeholder="example@example.com"
+              value={formValues.email}
+              required
+            />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="password">Password</label>
+            <input
+              onChange={handleChange}
+              type="password"
+              name="password"
+              value={formValues.password}
+              required
+            />
+          </div>
+          <button disabled={!formValues.email || !formValues.password}>
+            Login
+          </button>
+        </form>
+      </div>
 
       <h2>Register</h2>
-      <form onSubmit={handleRegister}>
-        <label>
-          Email:
-          <input
-            type="email"
-            value={registerEmail}
-            onChange={(e) => setRegisterEmail(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Password:
-          <input
-            type="password"
-            value={registerPassword}
-            onChange={(e) => setRegisterPassword(e.target.value)}
-            required
-          />
-        </label>
-        <button type="submit">Register</button>
-      </form>
+      <div className="card-overlay centered">
+        <form className="col" onSubmit={handleRegister}>
+          <div className="input-wrapper">
+            <label htmlFor="name">Name</label>
+            <input
+              onChange={handleChange}
+              name="name"
+              type="text"
+              placeholder="John Smith"
+              value={formValues.name}
+              required
+            />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="email">Email</label>
+            <input
+              onChange={handleChange}
+              name="email"
+              type="email"
+              placeholder="example@example.com"
+              value={formValues.email}
+              required
+            />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="password">Password</label>
+            <input
+              onChange={handleChange}
+              type="password"
+              name="password"
+              value={formValues.password}
+              required
+            />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input
+              onChange={handleChange}
+              type="password"
+              name="confirmPassword"
+              value={formValues.confirmPassword}
+              required
+            />
+          </div>
+          <button
+            disabled={
+              !formValues.email ||
+              !formValues.password ||
+              formValues.confirmPassword !== formValues.password
+            }
+          >
+            Register
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
