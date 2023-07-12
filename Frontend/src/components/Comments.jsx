1d2171eb1763c5b10e1a809bd7daa4958db8be75
Mayerling Joseph <josephmayerling@gmail.com>
2023-07-12 13:33:46 -0400

    Reading

--- Frontend/src/components/Comments.jsx
+++ Frontend/src/components/Comments.jsx
@@ -1,4 +1,24 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
+const Comments = () => {
+  const [comments, setComments] = useState([])
+
+  useEffect(() => {
+    const fetchComments = async () => {
+      const result = await axios.get('/comments')
+      setComments(result.data)
+    }
+
+    fetchComments()
+  }, [])
+
+  return (
+    <div>
+      {comments.map((comment, index) => (
+        <p key={index}>{comment.title}</p>
+      ))}
+    </div>
+  )
+}
 export default Comments
--- Frontend/src/components/Reviews.jsx
+++ Frontend/src/components/Reviews.jsx
@@ -1,4 +1,24 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
+const Reviews = () => {
+  const [reviews, setReviews] = useState([])
+
+  useEffect(() => {
+    const fetchReviews = async () => {
+      const result = await axios.get('/reviews')
+      setReviews(result.data)
+    }
+
+    fetchReviews()
+  }, [])
+
+  return (
+    <div>
+      {reviews.map((review, index) => (
+        <p key={index}>{review.title}</p>
+      ))}
+    </div>
+  )
+}
 export default Reviews
