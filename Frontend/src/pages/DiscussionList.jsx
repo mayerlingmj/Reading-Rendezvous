eff5bcd8de1c6a4795f7ccc732e06952fc46a26b
Mayerling Joseph <josephmayerling@gmail.com>
2023-07-16 06:53:31 -0400

    Reading

---
+++ Frontend/src/pages/DiscussionList.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { GetDiscussions } from '../services/Discussion'
+
+const DiscussionList = () => {
+  const [discussions, setDiscussions] = useState([])
+
+  useEffect(() => {
+    const fetchDiscussions = async () => {
+      const result = await GetDiscussions()
+      setDiscussions(result.data)
+    }
+
+    fetchDiscussions()
+  }, [])
+
+  return (
+    <div>
+      {discussions.map((discussion, index) => (
+        <Link key={index} to={`/discussiondetails/${discussion.id}`}>
+          <p>{discussion.title}</p>
+        </Link>
+      ))}
+    </div>
+  )
+}
+
+export default DiscussionList
