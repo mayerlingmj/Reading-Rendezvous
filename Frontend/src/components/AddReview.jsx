95db126c56fe700cb952ca9f3e10807282cda9fd
Mayerling Joseph <josephmayerling@gmail.com>
2023-07-19 09:31:47 -0400

    reading

--- Frontend/src/components/AddReview.jsx
+++ Frontend/src/components/AddReview.jsx
@@ -51,31 +51,64 @@ const AddReview = ({ user, bookId }) => {
   }
 
   return (
-    <form onSubmit={reviewId ? handleReviewUpdate : handleReviewSubmit}>
-      <label htmlFor="review">Review:</label>
-      <input
-        id="content"
-        value={review.content}
-        onChange={handleReviewChange}
-      />
-      <label htmlFor="rating">Rating:</label>
-      <input
-        id="rating"
-        type="number"
-        min="1"
-        max="5"
-        value={review.rating}
-        onChange={handleReviewChange}
-      />
-      <button type="submit">
-        {reviewId ? 'Update Review' : 'Submit Review'}
-      </button>
-      {reviewId && (
-        <button type="button" onClick={handleReviewDelete}>
-          Delete Review
-        </button>
-      )}
-    </form>
+    <div
+      style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}
+    >
+      <h2 style={{ width: '100%', textAlign: 'center' }}>Add Review</h2>
+      <div
+        style={{
+          width: '200px',
+          margin: '10px',
+          backgroundColor: 'lightpink',
+          borderRadius: '5px',
+          padding: '10px',
+          boxSizing: 'border-box'
+        }}
+      >
+        <form onSubmit={reviewId ? handleReviewUpdate : handleReviewSubmit}>
+          <label htmlFor="review" style={{ display: 'block' }}>
+            Review:
+          </label>
+          <textarea
+            id="content"
+            value={review.content}
+            onChange={handleReviewChange}
+            style={{ marginBottom: '10px', width: '100%', height: '100px' }}
+          />
+          <label htmlFor="rating" style={{ display: 'block' }}>
+            Rating:
+          </label>
+          <input
+            id="rating"
+            type="number"
+            min="1"
+            max="5"
+            value={review.rating}
+            onChange={handleReviewChange}
+            style={{ marginBottom: '10px', width: '100%' }}
+          />
+          <button type="submit">
+            {reviewId ? 'Update Review' : 'Submit Review'}
+          </button>
+          {reviewId && (
+            <button
+              type="button"
+              onClick={handleReviewDelete}
+              style={{
+                backgroundColor: 'hotpink',
+                color: 'white',
+                border: 'none',
+                borderRadius: '5px',
+                marginBottom: '10px',
+                width: '100%'
+              }}
+            >
+              Delete Review
+            </button>
+          )}
+        </form>
+      </div>
+    </div>
   )
 }
 
