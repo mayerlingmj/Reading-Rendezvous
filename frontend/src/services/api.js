785cde1d0b4325606e8ac9be7b719fbffa9ed75e
Mayerling Joseph <josephmayerling@gmail.com>
2023-07-19 13:49:21 -0400

    reading

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-// export const BASE_URL = 'http://localhost:3001'
-export const BASE_URL = ''
+export const BASE_URL = 'http://localhost:3001'
+// export const BASE_URL = ''
 
 const api = axios.create({ baseURL: BASE_URL })
 
