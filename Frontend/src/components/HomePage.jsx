b3260abbe70e4334758ef0950401ecfb37d7df34
Mayerling Joseph <josephmayerling@gmail.com>
2023-07-19 08:56:22 -0400

    Reading

--- Frontend/src/components/HomePage.jsx
+++ Frontend/src/components/HomePage.jsx
@@ -9,24 +9,38 @@ const HomePage = () => {
         backgroundImage: `url(${backgroundImage})`,
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat',
-        width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
         height: '100vh',
-        padding: '10px',
-        marginBottom: '20px'
+        padding: '10px'
       }}
     >
-      <h2>About Reading Rendezvous Book Club</h2>
-      <p>
-        Reading Rendezvous is a book club for those who love to immerse
-        themselves in literature. Our community is made up of passionate readers
-        and thinkers. We focus on exploring the works of a variety of authors,
-        from classic literature to contemporary pieces, and from well-known
-        writers to up-and-coming authors. We believe that reading is a journey,
-        and each book is an adventure that broadens our perspective and enriches
-        our understanding of the world. Our members come from all walks of life,
-        united by a common love for books. Join us on this exciting literary
-        journey and be a part of the Reading Rendezvous Book Club!
-      </p>
+      <div
+        style={{
+          backgroundColor: 'lightpink',
+          padding: '20px',
+          borderRadius: '15px',
+          maxWidth: '40%',
+          textAlign: 'center',
+          fontFamily: 'Papyrus, fantasy',
+          boxShadow: '0px 10px 15px rgba(0,0,0,0.1)'
+        }}
+      >
+        <h2>About Reading Rendezvous Book Club</h2>
+        <p>
+          Reading Rendezvous is a book club for those who love to immerse
+          themselves in literature. Our community is made up of passionate
+          readers and thinkers. We focus on exploring the works of a variety of
+          authors, from classic literature to contemporary pieces, and from
+          well-known writers to up-and-coming authors. We believe that reading
+          is a journey, and each book is an adventure that broadens our
+          perspective and enriches our understanding of the world. Our members
+          come from all walks of life, united by a common love for books. Join
+          us on this exciting literary journey and be a part of the Reading
+          Rendezvous Book Club!
+        </p>
+      </div>
     </div>
   )
 }
